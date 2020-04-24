353f60f632f06dfe97c64e5e58a9792b777612be
Alex Lillo <alex.lillo@gmail.com>
2020-04-24 10:57:14 +0100

    mwe

--- api/contact/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-$adminEmail =  "alex.lillo@gmail.com"; //Please change this to yours
-$SendMailFailederrorMessage = "Something went wrong";
-$SendMailEmptyerrorMessage = "Empty form";
--- src/App.js
+++ src/App.js
@@ -1,52 +1,21 @@
 import React from 'react';
-import './App.scss';
-
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom'
-
-import * as firebase from 'firebase/app'
-// import "firebase/auth";
-import {firebaseConfig} from './constants/firebase'
-import Header from './Components/Header'
-import Home from './pages/Home'
-import AddRace from './pages/admin/AddRace'
-import EditRace from './pages/admin/EditRace'
-import AdminIndex from './pages/admin/AdminIndex'
-import SignInScreen from './pages/admin/SignInScreen'
-import {AuthProvider} from './pages/admin/Auth'
-import PrivateRoute from './pages/admin/PrivateRoute'
-import Footer from './Components/Footer'
-import ContactForm from './pages/ContactForm'
-import TagManager from 'react-gtm-module'
-
-const tagManagerArgs = {
-    gtmId: 'GTM-MK8VHKD'
-}
-
-TagManager.initialize(tagManagerArgs)
-
-if(!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
 
 function App() {
+
+  console.log(process.env.REACT_APP_API_KEY);
+  console.log(process.env.NODE_ENV);
+  const api = process.env.REACT_APP_API_KEY;
+  const env = process.env.NODE_ENV;
+
+
+
   return (
     <div className="App">
-      <AuthProvider>
-        <Router>
-          <Header />
-            <Route exact path="/" component={Home}/>
-            <Route exact path="/signin" component={SignInScreen}/>
-            <Route exact path="/contact" component={ContactForm}/>
-            <PrivateRoute exact path="/race-control/" component={AdminIndex}/>
-            <PrivateRoute exact path="/race-control/add-race" component={AddRace}/>
-            <PrivateRoute exact path="/race-control/edit-race" component={EditRace}/>
-          <Footer/>
-        </Router>
-      </AuthProvider>
+      <h1>Meh 🍤</h1>
+      <ul>
+        <li>Environment is: {env}</li>
+        <li>API key is: {api}</li>
+      </ul>
     </div>
   );
 }
